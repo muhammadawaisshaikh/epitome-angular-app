70e92f3fa3714f64feb9a79df71b5d80394321e0
Muhammad Idrees <idreesiqbal104@gmail.com>
2021-02-18 14:25:37 +0500

    all ui done with responsive just 2 images not added

---
+++ src/assets/js/custom.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function topFunction() 
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
